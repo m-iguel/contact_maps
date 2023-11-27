fce0d647977c1b3229577e3fe1c0383a7b7da5c7
m-iguel <73526976+m-iguel@users.noreply.github.com>
2023-11-27 20:27:53 +0100

    Delete FrustraEvo_2023715141047268466 directory

--- FrustraEvo_2023715141047268466/Data/FrustraR.R
+++
@@ -1,4 +0,0 @@
-library(frustratometeR)
-PdbsDir <- '/root/FrustraEvo/FrustraEvo_2023715141047268466/Frustration/'
-ResultsDir <- '/root/FrustraEvo/FrustraEvo_2023715141047268466/Frustration/'
-dir_frustration(PdbsDir = PdbsDir, Mode = 'configurational', ResultsDir = ResultsDir,Graphics = FALSE)
